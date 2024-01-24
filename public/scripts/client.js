04baedc8315187c5e3e9eac86bc79112a60fc7a3
mariefranceperrier <mariefrance.perrier@gmail.com>
2024-01-24 09:26:15 -0500

    fetch tweets with AJAX + display time passed since a tweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,32 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
 const createTweetElement = function (tweet) {  
   let $tweet = $(`
     <article class="tweet">
@@ -51,7 +25,7 @@ const createTweetElement = function (tweet) {
       </div>
       <footer>
         <div class="tweet-date">
-          <h4>${tweet.created_at}</h4>
+          <h4>${timeago.format(tweet.created_at)}</h4>
         </div>
         <div class="tweet-actions">
           <i class="fa-solid fa-flag"></i>
@@ -74,13 +48,25 @@ const renderTweets = function (tweets) {
   }
 }
 
-renderTweets(data);
 
 $(function () {
       $('.new-tweet form').on('submit', function (event) {
         event.preventDefault();
         console.log('Button clicked, performing ajax call...');
-          
+        
+        const loadTweets = function () {
+          $.ajax({
+            url: '/tweets',
+            method: 'GET',
+            dataType: 'json',
+            success: (tweets) => {
+              renderTweets(tweets);
+            }
+          });
+        };
+
+        loadTweets();
+
         $.ajax({
           url: '/tweets',
           method: 'POST',
